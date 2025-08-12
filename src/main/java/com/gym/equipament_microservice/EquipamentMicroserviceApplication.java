3395f2043cca3f15fba40c2acbf55de1691ba604
MGMaikeru <13miguegonza2005@gmail.com>
2025-08-11 20:10:01 -0500

    initial commit

---
+++ src/main/java/com/gym/equipament_microservice/EquipamentMicroserviceApplication.java
@@ -0,0 +1,13 @@
+package com.gym.equipament_microservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EquipamentMicroserviceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EquipamentMicroserviceApplication.class, args);
+	}
+
+}
