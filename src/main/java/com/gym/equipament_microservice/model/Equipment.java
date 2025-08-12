0d3c05bdfb4c73e116709a224e7ec2c81f43190a
MGMaikeru <13miguegonza2005@gmail.com>
2025-08-12 16:02:37 -0500

    chore: add dockerfile statements

--- src/main/java/com/gym/equipament_microservice/model/Equipment.java
+++ src/main/java/com/gym/equipament_microservice/model/Equipment.java
@@ -3,8 +3,6 @@ package com.gym.equipament_microservice.model;
 import jakarta.persistence.*;
 import lombok.Data;
 
-import java.time.LocalDate;
-
 @Data
 @Entity
 @Table(name = "equipment")
--- src/main/java/com/gym/equipament_microservice/repository/EquipmentRepository.java
+++ src/main/java/com/gym/equipament_microservice/repository/EquipmentRepository.java
@@ -4,7 +4,6 @@ import com.gym.equipament_microservice.model.Equipment;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
-import java.time.LocalDate;
 import java.util.List;
 
 @Repository
